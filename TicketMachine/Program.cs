6006a18e767ed3f329ad210b1c8a60fca6fa4aa1
Alexey Zimarev <alex@zimarev.com>
2020-05-05 16:56:43 +0200

    Initial commit

---
+++ TicketMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicketMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
